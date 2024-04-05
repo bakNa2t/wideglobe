7c7af9083f122ec4022eac1a82b9e0ee7c0043ab
bakNa2t <ok2.lock@gmail.com>
2024-04-05 17:32:03 +0300

    Update City component for using localStorage

--- src/components/City.jsx
+++ src/components/City.jsx
@@ -1,10 +1,15 @@
-import { useParams } from "react-router-dom";
-import { useCities } from "../contexts/CitiesContext";
 import { useEffect } from "react";
-import styles from "./City.module.css";
-import Spinner from "./Spinner";
+import { useNavigate, useParams } from "react-router-dom";
+// import { useCities } from "../contexts/CitiesContext";
+import { useCitiesLocalStorage } from "../contexts/CitiesLocalStorageContext";
+import { useUrlPosition } from "../hooks/useUrlPosition";
+
+// import Spinner from "./Spinner";
 import BackButton from "./BackButton";
 import CountryFlag from "./CountryFlag";
+
+import styles from "./City.module.css";
+import Button from "./Button";
 
 const formatDate = (date) =>
   new Intl.DateTimeFormat("en", {
@@ -16,7 +21,11 @@ const formatDate = (date) =>
 
 function City() {
   const { id } = useParams();
-  const { getCity, currentCity, isLoading } = useCities();
+  // const { getCity, currentCity, isLoading } = useCities();
+  const { getCity, currentCity } = useCitiesLocalStorage();
+
+  const navigate = useNavigate();
+  const [lat, lng] = useUrlPosition();
 
   useEffect(
     function () {
@@ -27,7 +36,7 @@ function City() {
 
   const { cityName, emoji, date, notes } = currentCity;
 
-  if (isLoading) return <Spinner />;
+  // if (isLoading) return <Spinner />;
 
   return (
     <div className={styles.city}>
@@ -64,8 +73,16 @@ function City() {
         </a>
       </div>
 
-      <div>
+      <div className={styles.buttons}>
         <BackButton />
+        <Button
+          type="primary"
+          onClick={() =>
+            navigate(`/app/form?mode=edit&id=${id}&lat=${lat}&lng=${lng}`)
+          }
+        >
+          Edit
+        </Button>
       </div>
     </div>
   );

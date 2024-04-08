a268fc89c6d69186fc1a88797edf19a6110876d2
bakNa2t <ok2.lock@gmail.com>
2024-04-08 17:00:07 +0300

    Update Map component

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,7 +1,7 @@
-import { useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
-import { useCities } from "../contexts/CitiesContext";
-// import { useCitiesLocalStorage } from "../contexts/CitiesLocalStorageContext";
+import { useNavigate } from "react-router-dom";
+// import { useCities } from "../contexts/CitiesContext";
+import { useCitiesLocalStorage } from "../contexts/CitiesLocalStorageContext";
 import { useGeolocation } from "../hooks/useGeolocation";
 import { useUrlPosition } from "../hooks/useUrlPosition";
 import {
@@ -19,11 +19,12 @@ import styles from "./Map.module.css";
 import Button from "./Button";
 import CountryFlag from "./CountryFlag";
 import PropTypes from "prop-types";
+// import Sidebar from "./Sidebar";
 
 function Map() {
   const [mapPos, setMapPos] = useState([59.326, 18.073]);
-  const { cities } = useCities();
-  // const { cities } = useCitiesLocalStorage();
+  // const { cities } = useCities();
+  const { cities } = useCitiesLocalStorage();
   const {
     isLoading: isLoadingPosition,
     position: geolocationPosition,
@@ -31,6 +32,7 @@ function Map() {
   } = useGeolocation();
 
   const [mapLat, mapLng] = useUrlPosition();
+  // const [isSidebarOpen, setIsSidebarOpen] = useState(true);
 
   useEffect(
     function () {
@@ -48,43 +50,46 @@ function Map() {
   );
 
   return (
-    <div className={styles.mapContainer} /*onClick={() => navigate("form")}*/>
-      {!geolocationPosition && (
-        <Button type="position" onClick={getPosition}>
-          {isLoadingPosition ? "Loading..." : "Use your position"}
-        </Button>
-      )}
-      <MapContainer
-        center={mapPos}
-        zoom={10}
-        zoomControl={false}
-        scrollWheelZoom={true}
-        className={styles.map}
-      >
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
-        />
-        <ZoomControl position="bottomright" />
-        {cities.map((city) => {
-          return (
-            <Marker
-              position={[city.position.lat, city.position.lng]}
-              key={city.id}
-            >
-              <Popup>
-                <span>
-                  <CountryFlag emojiFlag={city.emoji} />
-                </span>
-                <span>{city.cityName}</span>
-              </Popup>
-            </Marker>
-          );
-        })}
-        <ChangeCenterPos position={mapPos} />
-        <DetectClick />
-      </MapContainer>
-    </div>
+    <>
+      {/* <Sidebar isSidebarOpen={isSidebarOpen} setIsSidebarOpen={setIsSidebarOpen}/> */}
+      <div className={styles.mapContainer} /*onClick={() => navigate("form")}*/>
+        {!geolocationPosition && (
+          <Button type="position" onClick={getPosition}>
+            {isLoadingPosition ? "Loading..." : "Use your position"}
+          </Button>
+        )}
+        <MapContainer
+          center={mapPos}
+          zoom={10}
+          zoomControl={false}
+          scrollWheelZoom={true}
+          className={styles.map}
+        >
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+          />
+          <ZoomControl position="bottomright" />
+          {cities.map((city) => {
+            return (
+              <Marker
+                position={[city.position.lat, city.position.lng]}
+                key={city.id}
+              >
+                <Popup>
+                  <span>
+                    <CountryFlag emojiFlag={city.emoji} />
+                  </span>
+                  <span>{city.cityName}</span>
+                </Popup>
+              </Marker>
+            );
+          })}
+          <ChangeCenterPos position={mapPos} />
+          <DetectClick />
+        </MapContainer>
+      </div>
+    </>
   );
 }
 

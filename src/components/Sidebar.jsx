d7826f1af73a523df985ba96543c6a0da1eaed68
bakNa2t <ok2.lock@gmail.com>
2024-04-09 15:49:51 +0300

    Added Footer comp to Sidebar comp

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,21 +1,18 @@
-import Logo from "./Logo";
-import AppNav from "./AppNav";
-import styles from "./Sidebar.module.css";
 import { Outlet } from "react-router-dom";
+
+import AppNav from "./AppNav";
+import Footer from "./Footer";
+import Logo from "./Logo";
+
+import styles from "./Sidebar.module.css";
 
 function Sidebar() {
   return (
     <div className={styles.sidebar}>
       <Logo />
       <AppNav />
-
       <Outlet />
-
-      <footer className={styles.footer}>
-        <p className={styles.copyright}>
-          &copy; Copyright {new Date().getFullYear()} by WideGlobe.
-        </p>
-      </footer>
+      <Footer />
     </div>
   );
 }

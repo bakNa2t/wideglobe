75ee50b895975ecd62de192ba15f4f08900dac30
bakNa2t <ok2.lock@gmail.com>
2024-04-10 15:47:15 +0300

    Added HiXMark icon btn to Sidebar comp

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,17 +3,23 @@ import { Outlet } from "react-router-dom";
 import AppNav from "./AppNav";
 import Footer from "./Footer";
 import Logo from "./Logo";
+import { HiXMark } from "react-icons/hi2";
 
 import styles from "./Sidebar.module.css";
 
 function Sidebar() {
   return (
-    <div className={styles.sidebar}>
-      <Logo />
-      <AppNav />
-      <Outlet />
-      <Footer />
-    </div>
+    <aside className={styles["sidebar-open"]}>
+      <div className={styles.sidebar}>
+        <Logo />
+        <AppNav />
+        <Outlet />
+        <Footer />
+        <button className={styles["sidebar-icon"]}>
+          <HiXMark />
+        </button>
+      </div>
+    </aside>
   );
 }
 

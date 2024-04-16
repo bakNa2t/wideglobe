9b4c6e5af5f561e9c64a92605e9817dae4067016
bakNa2t <ok2.lock@gmail.com>
2024-04-16 14:16:02 +0300

    Update content in Product page

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -8,17 +8,23 @@ export default function Product() {
       <section>
         <img src="img-1.jpg" alt="Waterfall" className={styles.img} />
         <div>
-          <h2>About WorldWide.</h2>
+          <h2>
+            About <em>WideGlobe</em>.
+          </h2>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo est
-            dicta illum vero culpa cum quaerat architecto sapiente eius non
-            soluta, molestiae nihil laborum, placeat debitis, laboriosam at fuga
-            perspiciatis?
+            This app allows users to search, mark and track their favorite
+            cities on a global map, making it easy to visualize past and future
+            destinations. With this feature, users can plan their trips
+            efficiently and keep a record of places they have visited. The app
+            offers a convenient way to create personalized itineraries and share
+            travel experiences with others.
           </p>
           <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis
-            doloribus libero sunt expedita ratione iusto, magni, id sapiente
-            sequi officiis et.
+            By providing a visual representetion of their journeys, users can
+            reminisce about past adventures and get inspired for new ones.
+            Ultimately, the app enhances the travel experience by allowing users
+            to map out their global adventures in a user-friendly and
+            interactive way.
           </p>
         </div>
       </section>
